14157aedf43bc2130e75a17cabfcb5d3d168ca81
Mykola Orliuk <virkony@gmail.com>
2026-06-27 23:49:07 +0200

    flake: replace flake-utils with flake-parts

--- flake.nix
+++ flake.nix
@@ -1,42 +1,59 @@
 {
   inputs = {
-    utils.url = "github:numtide/flake-utils";
-
     nixony.url = "github:ony/nixony";
-    nixony.inputs.nixpkgs.follows = "nixpkgs";
-    nixony.inputs.flake-utils.follows = "utils";
+    nixony.inputs = {
+      nixpkgs.follows = "nixpkgs";
+      flake-parts.follows = "flake-parts";
+      systems.follows = "systems";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, nixony }:
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      systems,
+      ...
+    }:
     let
-      pkgDefs = nixony.lib.mkPkgDefs (final0: {
+      pkgDefs = inputs.nixony.lib.mkPkgDefs (final0: {
         tree-sitter-grammars.tree-sitter-ion = final0.callPackage ./ion-grammar.nix { };
         vimPlugins.nvim-treesitter-ion = final0.callPackage ./nvim-plugin.nix { };
       });
-
-      out = system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
-        grammar = pkgs.tree-sitter-grammars.tree-sitter-ion;
-      in
-      {
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [
-            # extra dev tools
-          ];
-          inputsFrom = with pkgs; [ grammar ];
-        };
-
-        packages = pkgDefs.toFlatPackages pkgs;
-        defaultPackage = grammar;
-        checks = pkgDefs.toFlatPackages pkgs;
-      };
     in
-    with utils.lib;
-    {
-      overlays.default = final: prev: pkgDefs.toOverlay final prev;
-    } // eachSystem defaultSystems out;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake.overlays.default = pkgDefs.toOverlay;
+
+      systems = import systems;
+      perSystem =
+        {
+          self',
+          system,
+          pkgs,
+          ...
+        }:
+        {
+          # https://flake.parts/overlays.html?highlight=overlay#consuming-an-overlay
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+            config = { };
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              # extra dev tools
+            ];
+            inputsFrom = with pkgs; [
+              self'.packages.default
+            ];
+          };
+
+          packages = pkgDefs.toFlatPackages pkgs // {
+            default = pkgs.tree-sitter-grammars.tree-sitter-ion;
+          };
+          checks = self'.packages;
+        };
+    };
 }
