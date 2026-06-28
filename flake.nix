821dd30b48c7f148d665605abc05e4070f53f921
Mykola Orliuk <virkony@gmail.com>
2026-06-28 21:09:47 +0200

    flake: use nixony/packagesOverlay

--- flake.nix
+++ flake.nix
@@ -10,37 +10,29 @@
 
   outputs =
     inputs@{
-      self,
       nixpkgs,
       flake-parts,
       systems,
       ...
     }:
-    let
-      pkgDefs = inputs.nixony.lib.mkPkgDefs (final0: {
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.nixony.flakeModules.packagesOverlay
+      ];
+
+      packagesOverlay = final0: {
         tree-sitter-grammars.tree-sitter-ion = final0.callPackage ./ion-grammar.nix { };
         vimPlugins.nvim-treesitter-ion = final0.callPackage ./nvim-plugin.nix { };
-      });
-    in
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      flake.overlays.default = pkgDefs.toOverlay;
+      };
 
       systems = import systems;
       perSystem =
         {
           self',
-          system,
           pkgs,
           ...
         }:
         {
-          # https://flake.parts/overlays.html?highlight=overlay#consuming-an-overlay
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [ self.overlays.default ];
-            config = { };
-          };
-
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               # extra dev tools
@@ -50,10 +42,7 @@
             ];
           };
 
-          packages = pkgDefs.toFlatPackages pkgs // {
-            default = pkgs.tree-sitter-grammars.tree-sitter-ion;
-          };
-          checks = self'.packages;
+          packages.default = self'.packages.tree-sitter-ion;
         };
     };
 }
